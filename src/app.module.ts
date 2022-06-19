a3cd3eade18bc60ad41faee5e3757bfc27f8ffab
Max <zeetreex@gmail.com>
2022-06-19 21:41:04 +0300

    connected to pgsql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import {Module} from '@nestjs/common';
-import { TodosModule } from './todos/todos.module';
+import {TodosModule} from './todos/todos.module';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeConfigService} from "./config/sequelizeConfig.service";
+import {databaseConfig} from "./config/configuration";
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig]
+    }),
+    TodosModule
+  ],
   controllers: [],
   providers: [],
 })
